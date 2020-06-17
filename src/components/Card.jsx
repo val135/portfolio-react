142fceee7787684739c5a43343428c7b8730fff8
Valentina <valen_135@hotmail.com>
2020-06-17 13:55:38 -0500

    more cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ function Card(props) {
     return (
         
         <>
-             <div className="card d-flex-md-row project-container mx-3 px-3 pt-3 mb-3 pb-1 rounded"> <a name="targetprojects"></a>
+             <div className="card d-flex-md-row project-container mx-3 px-3 pt-4 mb-3 pb-1 rounded"> <a name="targetprojects"></a>
                 <img className="card-img-top project-pic border-left border-right border-bottom" src={props.img} alt="Card image cap" />
                 <div className="card-body mx-1 px-1">
                     <h5 className="card-title"><b>{props.titulo}</b></h5>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,15 +8,7 @@ class Cards extends React.Component {
             <div className="container-fluid">              
                 <h3 className="text-center selection-work-text">Here you can see some projects I have done!</h3>
                 <div className="row container-fluid web-projects mx-auto justify-content-center w-100"> 
-                    {/*
-                    <Card
-                    img = {'/lecarne.png'}
-                    titulo={'Landing product'}
-                    description = {'Landing page product designed for Le Carné Gourmet, a restaurant located in Medellín, Colombia'}
-                    urlButtom = {'https://val135.github.io/landing-product/'}
-                    />
-                    */}
-
+                    
                     <Card
                     img = {'/wikipedia.png'}
                     titulo={'Wikipedia Clone'}
@@ -29,13 +21,6 @@ class Cards extends React.Component {
                     titulo={'Documentation Page'}
                     description = {'Documentation Page with important information about Medellín. It was created for Viajecdotas, a personal travel blog.'}
                     urlButtom = {'https://val135.github.io/documentation-page/'}
-                    />
-
-                    <Card
-                    img = {'/packy-food.png'}
-                    titulo={'Website design'}
-                    description = {'PackyFood is an application that fights waste food in Colombia. Clic here to see more.'}
-                    urlButtom = {'https://packyfood.netlify.app/'}
                     />
 
                     <Card
@@ -58,15 +43,46 @@ class Cards extends React.Component {
                     description = {'The famous rock, paper and scissors game created using Vanilla JavaScrip. Clic here to play!'}
                     urlButtom = {'https://rock-paper-scissors-val135.netlify.app/'}
                     />
-                    
-                    {/*
+
+                    {
                     <Card
                     img = {'/calculator.png'}
                     titulo={'Calculator'}
                     description = {'This is a Free Code Camp project made in Vanilla JavaScript. It works as a calculator to add, multiply, divide and more. '}
                     urlButtom = {'https://gifted-swirles-3016d5.netlify.com'}
                     />
-                    */}
+                    }
+
+                    <Card
+                    img = {'/packy-food.png'}
+                    titulo={'Website design'}
+                    description = {'PackyFood is an application that fights waste food in Colombia. Clic here to see more.'}
+                    urlButtom = {'https://packyfood.netlify.app/'}
+                    />
+
+                    {
+                    <Card
+                    img = {'/lecarne.png'}
+                    titulo={'Website design'}
+                    description = {'Redesing of Le Carné Website to implement an ecommerce and show all their products and restaurant '}
+                    urlButtom = {'https://www.lecarnegourmet.com/nuevositio'}
+                    />
+                    }
+
+{
+                    <Card
+                    img = {'/agile-innova.jpg'}
+                    titulo={'Website design'}
+                    description = {'Redesing of Agile Innova Website, a company that improves businesses innovation  '}
+                    urlButtom = {'https://www.lecarnegourmet.com/nuevositio'}
+                    />
+                    }
+
+                    
+
+                    
+                    
+                    
 
                 </div>
             </div>        
